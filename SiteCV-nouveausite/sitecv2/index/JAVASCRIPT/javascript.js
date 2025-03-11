695835824329c621274ccc41d1249058350d85e9
MDax <maximederenes@gmail.com>
2025-03-11 15:42:02 +0100

    miseajour javascrypt

--- SiteCV-nouveausite/sitecv2/index/JAVASCRIPT/javascript.js
+++ SiteCV-nouveausite/sitecv2/index/JAVASCRIPT/javascript.js
@@ -1,7 +1,8 @@
-
-document.getElementById('scrollToTop').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('#top').scrollIntoView({
-        behavior: 'smooth'
+document.querySelectorAll('.scrollToTop').forEach(function(element) {
+    element.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('#top').scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
